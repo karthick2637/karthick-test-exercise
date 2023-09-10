f24e9e0de1b28bc24b7590609f146dc00c6561d5
Karthick <karthick2637@gmail.com>
2023-09-10 23:32:10 +0530

    second commit

--- cypress/support/PageObjects/CartPage.js
+++ cypress/support/PageObjects/CartPage.js
@@ -1,30 +1,37 @@
 class CartPage {
   elements = {
-    productsAdded: () => cy.get("td.product-name"),
+    productsAdded: () => cy.get('input[title="Qty"]'),
     productPrice: () =>
       cy.get("td.product-price span.woocommerce-Price-amount.amount"),
+    removeItem : () => cy.get('a[aria-label="Remove this item"]')
   };
 
   itemsInCartValidation(expectedCount) {
-    this.elements.productsAdded().should("have.length", expectedCount);
-  }
+    this.elements.productsAdded().then(($qty) => {
+      const value = Cypress._.map($qty, "value");
+      const valueInNumberic = value.map((val) => Number(val));
+      let total = valueInNumberic.reduce((sum, val)=> sum+val,0)
+      expect(total).to.equal(expectedCount)
+  })
+}
 
   removeLowestPriceItem() {
     this.elements.productPrice().then(($price) => {
       const priceText = Cypress._.map($price, "innerText");
       const itemPrice = priceText.map((price) => price.substring(1));
       const priceInNumberic = itemPrice.map((val) => Number(val));
-      const lowestPriceItem = Math.min(priceInNumberic);
+      const lowestPriceItem = Math.min.apply(Math, priceInNumberic);
+      cy.log(lowestPriceItem)
 
       this.elements.productPrice().each(($el, index, $list) => {
         const priceText = $el.text();
         const itemPrice = Number(priceText.substring(1));
         if (itemPrice == lowestPriceItem) {
           this.elements
-            .productPrice()
+            .removeItem()
             .eq(index)
-            .prevUntil("td a.remove")
             .click();
+            cy.wait(2000)
         }
       });
     });
